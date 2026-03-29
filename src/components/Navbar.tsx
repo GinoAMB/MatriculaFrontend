4bacd336c3eec24e7513674f94e4a7939f03b1da
GinoAMB <gino.anderson2011@gmail.com>
2026-03-29 18:31:36 -0500

    Se agrego opciones al nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
 import { HiAcademicCap } from "react-icons/hi2";
-import { FiX } from "react-icons/fi";
+import {
+  FiX,
+  FiHome,
+  FiSettings,
+  FiFileText,
+  FiGlobe
+} from "react-icons/fi"; 
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { FaHouseMedicalCircleExclamation } from "react-icons/fa6";
+import { FaListCheck, FaUserPlus } from "react-icons/fa6";
+import { FiChevronDown } from "react-icons/fi";
+import { FaUserCog } from "react-icons/fa";
+import { LuUserRoundCog } from "react-icons/lu";
+
 
 export default function Navbar({ open, setOpen }: any) {
+  const [openConfig, setOpenConfig] = useState(false);
+  const [openUsers, setOpenUsers] = useState(false);
   return (
     <aside
       className={`
@@ -43,28 +58,133 @@ export default function Navbar({ open, setOpen }: any) {
         <NavLink
           to="/admin/dashboard"
           className={({ isActive }) =>
-            `px-3 py-2 rounded-md transition ${
-              isActive
-                ? "bg-gray-100 md:bg-white text-primary shadow"
-                : "hover:bg-gray-100 md:hover:bg-white"
+            `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+              ? "bg-gray-100 md:bg-white text-primary shadow"
+              : "hover:bg-gray-100 md:hover:bg-white"
             }`
           }
         >
+          <FiHome />
           Panel de Control
         </NavLink>
 
-        <NavLink
-          to="/admin/users"
-          className={({ isActive }) =>
-            `px-3 py-2 rounded-md transition ${
-              isActive
-                ? "bg-gray-100 md:bg-white text-primary shadow"
-                : "hover:bg-gray-100 md:hover:bg-white"
-            }`
-          }
-        >
-          Gestión de Usuarios
-        </NavLink>
+        <div>
+          <button
+            onClick={() => setOpenUsers(!openUsers)}
+            className="w-full flex items-center gap-2 px-3 py-2 rounded-md transition hover:bg-gray-100 md:hover:bg-white cursor-pointer"
+          >
+            <FaUserCog />
+
+            <span>Gestión de Usuarios</span>
+
+            <FiChevronDown
+              className={`ml-auto transition-transform duration-300 ${openUsers ? "rotate-180" : "rotate-0"
+                }`}
+            />
+          </button>
+
+          {/* SUBMENÚ */}
+          {openUsers && (
+            <div className="ml-4 mt-1 flex flex-col gap-2 text-sm">
+
+              <NavLink
+                to="/admin/users"
+                className={({ isActive }) =>
+                  `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+                    ? "bg-gray-100 md:bg-white text-primary shadow"
+                    : "hover:bg-gray-100 md:hover:bg-white"
+                  }`
+                }
+              >
+                <FaUserPlus />
+                Usuarios
+              </NavLink>
+
+              <NavLink
+                to="/admin/roles"
+                className={({ isActive }) =>
+                  `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+                    ? "bg-gray-100 md:bg-white text-primary shadow"
+                    : "hover:bg-gray-100 md:hover:bg-white"
+                  }`
+                }
+              >
+                <LuUserRoundCog />
+                Roles
+              </NavLink>
+
+            </div>
+          )}
+        </div>
+
+        {/* CONFIGURACIÓN */}
+        <div>
+          <button
+            onClick={() => setOpenConfig(!openConfig)}
+            className="w-full flex items-center gap-2 px-3 py-2 rounded-md transition hover:bg-gray-100 md:hover:bg-white cursor-pointer"
+          >
+            <FiSettings />
+
+            <span>Configuración</span>
+
+            {/* FLECHA */}
+            <FiChevronDown
+              className={`ml-auto transition-transform duration-300 ${openConfig ? "rotate-180" : "rotate-0"
+                }`}
+            />
+          </button>
+
+          {/* SUBMENÚ */}
+          {openConfig && (
+            <div className="ml-4 mt-1 flex flex-col gap-2 text-sm">
+              <NavLink to="/admin/tipo-documento" className={({ isActive }) =>
+                `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+                  ? "bg-gray-100 md:bg-white text-primary shadow"
+                  : "hover:bg-gray-100 md:hover:bg-white"
+                }`
+              }>
+                <FiFileText />
+                Tipos de Documento
+              </NavLink>
+
+              <NavLink
+                to="/admin/religiones"
+                className={({ isActive }) =>
+                  `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+                    ? "bg-gray-100 md:bg-white text-primary shadow"
+                    : "hover:bg-gray-100 md:hover:bg-white"
+                  }`
+                }
+              >
+                <FaHouseMedicalCircleExclamation />
+                Religiones
+              </NavLink>
+
+              <NavLink to="/admin/paises" className={({ isActive }) =>
+                `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+                  ? "bg-gray-100 md:bg-white text-primary shadow"
+                  : "hover:bg-gray-100 md:hover:bg-white"
+                }`
+              }>
+                <FiGlobe />
+                Países
+              </NavLink>
+
+              <NavLink
+                to="/admin/estado-matricula"
+                className={({ isActive }) =>
+                  `flex items-center gap-2 px-3 py-2 rounded-md transition ${isActive
+                    ? "bg-gray-100 md:bg-white text-primary shadow"
+                    : "hover:bg-gray-100 md:hover:bg-white"
+                  }`
+                }
+              >
+                <FaListCheck />
+                Estados de Matrícula
+              </NavLink>
+            </div>
+          )}
+        </div>
       </nav>
     </aside>
   );
